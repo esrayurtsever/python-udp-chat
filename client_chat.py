ef468158a296c6e62b68ba14fea9b07de6047b07
Esra Yurtsever <esrayurtsever>
2026-08-04 09:31:35 +0300

    Initial commit - UDP Chat Application

---
+++ client_chat.py
@@ -0,0 +1,24 @@
+﻿import socket
+
+address =  socket.gethostbyname(socket.gethostname())
+port = 5000
+byte = 1024
+client =socket.socket(socket.AF_INET ,socket.SOCK_DGRAM)#udp de tek socket yeterlidir bağlantı kurulmadığı için 
+
+#udp clientta bind yapılmaz çünkü portu dinlemeye ihtiyacı yoktur sendto ile veri göndermeye ihtiyacı var
+
+while True:
+    message=input("text: ")
+
+    client.sendto(message.encode("utf-8"), (address,port ))
+    
+    if message == "quit":
+        break
+    else:
+       
+        #sendto ya göndermesini istediğimiz verileri veririz sendto bize veri vermez
+      
+        message ,address = client.recvfrom(byte)
+        message=message.decode("utf-8")
+        print(f"text: { message}")
+client.close()
---
+++ server_chat.py
@@ -0,0 +1,23 @@
+﻿import socket #ağ programlama yapıcaksan akla ilk socket modülü gelir internet üzerindeki veri iletişmin temelidir
+address =  socket.gethostbyname(socket.gethostname())
+port = 5000
+byte = 1024
+server =socket.socket(socket.AF_INET ,socket.SOCK_DGRAM)#udp de tek socket yeterlidir bağlantı kurulmadığı için 
+
+server.bind((address, port))#gelen paketleri işletim sistemine söylüyor bu ip ve porttan gelcek udp paketlerini dinlicem demek 
+#bind fonksiyonuna tek bir argüman gönderiyorum. Bu argümanın içinde iki bilgi var: IP ve port.
+#server.bind(address, port) şu anlama gelir: bind fonksiyonuna 2 ayrı argüman gönderiyorum. Ama bind() bunu kabul etmez.
+ #socketler ağ üzerinden string gönderemez her aslında byte dizisidir
+#karşı taraftan gelen mesajı almak için bu fonku kullandım benim ona cevap vermem için bana gönderdiği mesaj ve gönderenin adresi lazım
+#message yi fonksiyonun döndürdüğü ilk değeri bi yere koymam gerektiği için kullandım
+print("Server is working...")
+while True:
+    message ,address = server.recvfrom(byte)
+    message=message.decode("utf-8")
+    if message == "quit":
+        break
+    else:
+        print(f"text: { message}")
+        message = input("text: ")
+        server.sendto(message.encode("utf-8"), address) #sendto ya göndermesini istediğimiz verileri veririz sendto bize veri vermez
+server.close
